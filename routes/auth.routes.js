ce8ca0f8f6ff20f0298ddd5f2933acf179bd32fa
Marcel Łukomski <marcel.lukomski@gmail.com>
2020-11-20 19:38:05 +0100

    Implement OAuth and separate code

---
+++ routes/auth.routes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const passport = require('passport');
+const router = express.Router();
+
+router.get('/auth/google', 
+  passport.authenticate('google', { scope: ['email', 'profile'] }));
+
+router.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/user/no-permission'}),
+ (req, res) => {
+  res.redirect('/user/logged');
+});
+
+module.exports = router;
